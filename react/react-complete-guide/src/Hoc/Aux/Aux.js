b8cd00d61790cd170e0facee51e6a65a9e082383
Sushil Kumar <sushil.kumar@thoughtworks.com>
2019-12-14 15:01:41 +0530

    delete

--- react/react-complete-guide/src/Hoc/Aux/Aux.js
+++
@@ -1,8 +0,0 @@
-// used when you need to return multipe adjecent elements 
-// but dont want to encole inside a wrapping div
-// its equivalent to one React.createElement() call inside component you use it
-// another way is using Reacts own Fragment component to wrap your content
-
-const Aux = props => props.children
-
-export default Aux;
